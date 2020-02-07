963b9731e978638be61e80f4cfb1e6b30e95b5bf
LOGAN LEE <dlfjgkssk1@naver.com>
2020-02-07 21:18:58 +0900

    - Add util.js  sort preProcessedList.

--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -1,6 +1,6 @@
 import store from 'store';
 import * as actions from 'actions';
-import { orderStandard } from 'internal_constants';
+import { NameOfOrderStandard } from 'internal_constants';
 import moment from 'moment';
 
 export function preListSorter(preProcessedList) {
@@ -10,106 +10,35 @@ export function preListSorter(preProcessedList) {
 	list.sort((a, b) => {
 		return moment(a.createdAt) - moment(b.createdAt);
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.createdAt, true));
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.createdAt, true));
 
 	//CREATE_AT_DESC
 	list.sort((a, b) => {
 		return moment(b.createdAt) - moment(a.createdAt);
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.createdAt, false));
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.createdAt, false));
 
 	//HITCOUNT_ASC
 	list.sort((a, b) => {
 		return a.hitCount - b.hitCount;
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.hitCount, true));
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.hitCount, true));
 
 	//HITCOUNT_DESC
 	list.sort((a, b) => {
 		return b.hitCount - a.hitCount;
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.hitCount, false));
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.hitCount, false));
 
 	//REGISTERED_AT_ASC
 	list.sort((a, b) => {
 		return moment(a.registeredAt) - moment(b.registeredAt);
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.registeredAt, true));
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.registeredAt, true));
 
 	//REGISTERED_AT_DESC
 	list.sort((a, b) => {
 		return moment(b.registeredAt) - moment(a.registeredAt);
 	});
-	store.dispatch(actions.SET_SORTED_LIST(list, orderStandard.registeredAt, false));
-}
-
-export function preListSorterTester(preProcessedList) {
-	const list = [...preProcessedList];
-	console.info('preListSorterTester');
-
-	//CREATE_AT_ASC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return moment(a.createdAt) - moment(b.createdAt);
-			}),
-			orderStandard.createdAt,
-			true,
-		),
-	);
-
-	//CREATE_AT_DESC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return moment(b.createdAt) - moment(a.createdAt);
-			}),
-			orderStandard.createdAt,
-			false,
-		),
-	);
-
-	//HITCOUNT_ASC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return a.hitCount - b.hitCount;
-			}),
-			orderStandard.hitCount,
-			true,
-		),
-	);
-
-	//HITCOUNT_DESC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return b.hitCount - a.hitCount;
-			}),
-			orderStandard.hitCount,
-			false,
-		),
-	);
-
-	//REGISTERED_AT_ASC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return moment(a.registeredAt) - moment(b.registeredAt);
-			}),
-			orderStandard.registeredAt,
-			true,
-		),
-	);
-
-	//REGISTERED_AT_DESC
-	store.dispatch(
-		actions.SET_SORTED_LIST(
-			list.sort((a, b) => {
-				return moment(b.registeredAt) - moment(a.registeredAt);
-			}),
-			orderStandard.registeredAt,
-			false,
-		),
-	);
+	store.dispatch(actions.SET_SORTED_LIST(list, NameOfOrderStandard.registeredAt, false));
 }
