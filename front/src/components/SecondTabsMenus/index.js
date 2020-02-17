6b83da0a893437a130f18d3237ceaa8cfca66f9f
LOGAN LEE <dlfjgkssk1@naver.com>
2020-02-17 14:27:59 +0900

    - Add constants for firstMenu and secondMenu
    - Mod state name of ui menus

--- front/src/components/SecondTabsMenus/index.js
+++ front/src/components/SecondTabsMenus/index.js
@@ -6,25 +6,27 @@ import * as actions from 'actions';
 
 import Buttons from '../Buttons';
 import Wrapper from './Wrapper';
+import { SECOND_MENU } from 'internal_constants';
 
 function tabHandler(e, value) {
 	store.dispatch(actions.secondTabMover(value));
 }
 
-export default function SecondTabsMenus({ currentSecondTabId }) {
+export default function SecondTabsMenus({ secondMenu }) {
 	return (
 		<Wrapper>
 			<Tabs
-				value={currentSecondTabId}
+				value={secondMenu}
 				onChange={(e, value) => tabHandler(e, value)}
 				indicatorColor='primary'
 				textColor='primary'
 				variant='fullWidth'
-				aria-label='full width tabs example'>
-				<Tab value='ALL' label='ALL' />
-				<Tab value='TOP100' label='TOP100' />
-				<Tab label='TEST3' />
-				<Tab label='TEST4' />
+				aria-label='full width tabs example'
+			>
+				<Tab value={SECOND_MENU.ALL} label={SECOND_MENU.ALL} />
+				<Tab value={SECOND_MENU.TOP100} label={SECOND_MENU.TOP100} />
+				<Tab value={SECOND_MENU.TEST3} label={SECOND_MENU.TEST3} />
+				<Tab value={SECOND_MENU.TEST4} label={SECOND_MENU.TEST4} />
 			</Tabs>
 		</Wrapper>
 	);
